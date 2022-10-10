3c0df60dc7a969aea07a7080cedb593c1201f5b9
Abir Das <abirdas422@gmail.com>
2022-10-10 22:25:51 +0600

    connecting with git and installing react-router, bootstrap and fontawesome

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
