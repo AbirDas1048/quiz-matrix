d3081d25aad8de6fb9970061a3de272f849933c8
Abir Das <abirdas422@gmail.com>
2022-10-12 01:20:56 +0600

    Toster added and show message of selected answer in Toaster

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Card, Col, Form } from 'react-bootstrap';
+import { Button, Card, Col, Form } from 'react-bootstrap';
 import './Option.css';
 
-const Option = ({ option, questionId, optionId }) => {
+const Option = ({ option, questionId, optionId, selectedAnswer }) => {
     const radioId = questionId + "-" + optionId;
     return (
         <Col>
             <Card className='h-100 option-card'>
                 <Card.Body>
-                    <Form.Check type='radio' id={radioId} className="h-100" >
-                        <Form.Check.Input type='radio' name={questionId} className="" />
-                        <Form.Check.Label className='w-100 h-100'>{option}</Form.Check.Label>
+                    <Form.Check type='radio' id={radioId} >
+                        <Form.Check.Input onClick={() => selectedAnswer(option)} type='radio' name={questionId} className="" />
+                        <Form.Check.Label className="w-100">{option}</Form.Check.Label>
                     </Form.Check>
                 </Card.Body>
             </Card>
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -3,10 +3,24 @@ import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 import React, { useState } from 'react';
 import { Card, Form, Row } from 'react-bootstrap';
 import Option from '../Option/Option';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Question = ({ qs, quizNo }) => {
     const { id, question, options, correctAnswer } = qs;
     const [showAnswer, setShowAnswer] = useState(false);
+    const selectedAnswer = (answer) => {
+
+        answer === correctAnswer ? toast.success("Correct Answer") : toast.error("Wrong Answer")
+        // toast(answer);
+        // toast.success("Success Notification !", {
+        //     position: toast.POSITION.TOP_CENTER
+        // });
+
+        // toast.error("Error Notification !", {
+        //     position: toast.POSITION.TOP_LEFT
+        // });
+    }
     return (
         <div>
             <Card className='mb-5 shadow'>
@@ -19,7 +33,7 @@ const Question = ({ qs, quizNo }) => {
                     <Form>
                         <Row lg={2} className="g-4 justify-content-center mb-3">
                             {
-                                options.map((option, idx) => <Option key={idx} option={option} questionId={id} optionId={idx}></Option>)
+                                options.map((option, idx) => <Option key={idx} option={option} questionId={id} optionId={idx} selectedAnswer={selectedAnswer}></Option>)
                             }
                         </Row>
                     </Form>
@@ -29,6 +43,7 @@ const Question = ({ qs, quizNo }) => {
 
                 </Card.Body>
             </Card>
+            <ToastContainer></ToastContainer>
 
         </div>
     );
